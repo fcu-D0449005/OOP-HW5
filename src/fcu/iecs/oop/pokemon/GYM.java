e6748083a56de0af8fb7f9a96cfda636fb0723f3
fcu-D0449005 <austria5533@gmail.com>
2016-11-30 17:02:13 +0800

    作業五

---
+++ src/fcu/iecs/oop/pokemon/GYM.java
@@ -0,0 +1,14 @@
+package fcu.iecs.oop.pokemon;
+
+
+
+
+public class GYM {
+	public static void fight( Pokemon p1,  Pokemon p2) {
+		
+		   
+		   System.out.println("Winner is Pokemon p"+(int)(Math.random()*2+1)+" cp+500");
+	}
+	
+
+}
