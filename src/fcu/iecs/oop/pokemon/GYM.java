0bb3e3f27989a0e92f4590893013b87b6eef360a
fcu-D0449005 <austria5533@gmail.com>
2016-11-30 17:47:12 +0800

    作業五

--- src/fcu/iecs/oop/pokemon/GYM.java
+++ src/fcu/iecs/oop/pokemon/GYM.java
@@ -7,23 +7,23 @@ public class GYM {
 
 	
 	public static void fight( Pokemon p1,  Pokemon p2) {
-		int cp = 0;
+		
 		   
 		if((int)(Math.random()*2+1)==1)
 		{
-			 p1 = new Pokemon("Psyduck", 100);
-			 cp=cp+500;
-			 System.out.println("Winner is Psyduck");
-			 System.out.println(100+cp);
+			
+			 System.out.println("Winner is "+p1.getName());
+			 p1.setCp(100);
+			 System.out.println(p1.getCp()+500); 
 		}
 		else
 		{
-		     p2 = new Pokemon("Pikachu", 300);
-			 cp=cp+500;
-			 System.out.println("Winner is Pikachu");
-			 System.out.println(300+cp);
+		
+			 System.out.println("Winner is "+p2.getName());
+			 p2.setCp(300);
+			 System.out.println(p2.getCp()+500);
 		}
 	}
 
-	
+
 }
